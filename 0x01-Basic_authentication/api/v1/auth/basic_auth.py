5de0f33e3e6cf7bdce99b11caacd086d85ea16d9
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-02-25 16:52:43 +0200

    Task 7. Basic - Base64 part

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''Basic Authentication Class'''
+import re
+import base64
+import binascii
+from typing import Tuple, TypeVar
+
+from .auth import Auth
+from models.user import User
+
+
+class BasicAuth(Auth):
+    '''Basic auth'''
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        '''base64 authorization header extraction'''
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+            return None
+        
