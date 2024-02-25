e375344275f8aae9efe8a2df07bc13663077f953
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-02-25 15:03:06 +0200

    Task 6. Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     '''hashing using bcrypt'''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''password validity'''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
