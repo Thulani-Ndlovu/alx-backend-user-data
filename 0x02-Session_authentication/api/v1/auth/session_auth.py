a86961e72865ede39276e7e8c286ae6681f62147
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-02-25 22:31:23 +0200

    Task 2. Create session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 '''session authentication'''
+from uuid import uuid4
+from flask import request
 
-from auth import Auth
+from .auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
     '''SessionAuth Class'''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''creating user session'''
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id        
