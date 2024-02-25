b9bc6b5b9970bbdae94fe1dc175f6b29af132a35
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-02-25 22:01:29 +0200

    Task 0. Et moi et moi et moi

---
+++ 0x02-Session_authentication/README.md
@@ -0,0 +1,11 @@
+0x01. Basic authentication Project By Thulani Ndlovu
+====================================================
+Project Learning Objectives
+1. What authentication means
+2. What Base64 is
+3. How to encode a string in Base64
+4. What Base authentication means
+5. How to send the Authorization header
+
+Tasks
+- This project consists of 12 Mandatory tasks and 2 advanced(optional) tasks that help in achieving the learning objectives listed above.
---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'''API Authentication'''
+import re
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    '''Authentication class'''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''authorization request'''
+        if path is not None and excluded_paths is not None:
+            for ex_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if ex_path[-1] == '*':
+                    pattern = '{}.*'.format(ex_path[0:-1])
+                elif ex_path[-1] == '/':
+                    pattern = '{}/*'.format(ex_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(ex_path)
+                if re.match(pattern, path):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        '''authorization header'''
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''current user'''
+        return None
