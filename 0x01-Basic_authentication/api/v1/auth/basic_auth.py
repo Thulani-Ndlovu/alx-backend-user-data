3558294ea3cae421b2af3cdda8f7b96e94eb5a53
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-02-25 17:04:26 +0200

    Task 8. Basic - Basic64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,10 +15,23 @@ class BasicAuth(Auth):
             self,
             authorization_header: str) -> str:
         '''base64 authorization header extraction'''
-        if type(authorization_header) == str:
+        if type(authorization_header) is str:
             pattern = r'Basic (?P<token>.+)'
             field_match = re.fullmatch(pattern, authorization_header.strip())
             if field_match is not None:
                 return field_match.group('token')
             return None
-        
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        '''decode base 64 authorization header'''
+        if type(base64_authorization_header) is str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
