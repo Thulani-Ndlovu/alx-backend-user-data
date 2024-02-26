346f601e69cfbf8eb4e236ae087580a2caa5a170
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-02-26 12:56:16 +0200

    Task 10. Sessions in database

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''User session class'''
+from models.base import Base
+
+
+class UserSession(Base):
+    '''User session'''
+    def __init__(self, *args: list, **kwargs: dict):
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
