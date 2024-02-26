9424217d46b912119d19d9a17bdbaaf2027b56b6
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-02-26 11:07:03 +0200

    Task 6. Use Session ID for identifying a User

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,5 +25,5 @@ class SessionAuth(Auth):
 
     def current_user(self, request=None):
         '''returns user instance based on a cookie value'''
-        user_id = self.user_id_by_session_id(self.session_cookie(request))
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
         return User.get(user_id)
