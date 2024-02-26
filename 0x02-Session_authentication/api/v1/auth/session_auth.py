f64371c78b701d7c7e5c8b4f3ba4f10c059f55a2
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2024-02-26 10:13:01 +0200

    Task 3. User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -16,4 +16,9 @@ class SessionAuth(Auth):
         if type(user_id) is str:
             session_id = str(uuid4())
             self.user_id_by_session_id[session_id] = user_id
-            return session_id        
+            return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        '''get user id with a certain session id'''
+        if type(session_id) is str:
+            return self.user_id_by_session_id.get(session_id)
